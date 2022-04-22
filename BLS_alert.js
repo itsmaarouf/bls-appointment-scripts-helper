544a890821f72f0a78347b2dba7df3f2527c5a28
Hamza El Maarouf <78151477+itsmaaroufwork@users.noreply.github.com>
2022-04-22 15:00:43 +0000

    Update BLS_alert.js

--- BLS_alert.js
+++ BLS_alert.js
@@ -4,15 +4,12 @@
 // @version      2.0.3
 // @description  bls aler  P|R|C Clean Refresh Alarm Notif
 // @author       Mous@2021
-// @match        https://morocco.blsspainvisa.com/appointment_family.php
-// @match        https://morocco.blsspainvisa.com/appointment.php
-// @match        https://morocco.blsspainvisa.com/embassy_appointment.php
-// @match        https://morocco.blsspainvisa.com/spanish/appointment.php
-// @match        https://morocco.blsspainvisa.com/english/appointment_family.php
-// @match        https://morocco.blsspainvisa.com/spanish/appointment.php
-// @match        https://morocco.blsspainvisa.com/spanish/appointment_family.php
-// @match        https://morocco.blsspainvisa.com/embassy_appointment.php
-// @match        https://morocco.blsspainvisa.com/english/embassy_appointment.php
+// @match        *://*.blsspainvisa.com/appointment_family.php
+// @match        *://*.blsspainvisa.com/*/appointment_family.php
+// @match        *://*.blsspainvisa.com/appointment.php
+// @match        *://*.blsspainvisa.com/*/appointment.php
+// @match        *://*.blsspainvisa.com/embassy_appointment.php
+// @match        *://*.blsspainvisa.com/*/embassy_appointment.php
 // @grant        none
 // ==/UserScript==
 
