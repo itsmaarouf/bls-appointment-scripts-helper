cc207b01c508858de7b5ccad8b6e883708cd8b87
Hamza El Maarouf <78151477+itsmaaroufwork@users.noreply.github.com>
2022-05-11 14:22:54 +0100

    Update Auto_Page_Reload_on_Error_and_Gateway_Problem_Fixer.js

--- Auto_Page_Reload_on_Error_and_Gateway_Problem_Fixer.js
+++ Auto_Page_Reload_on_Error_and_Gateway_Problem_Fixer.js
@@ -4,13 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        *://*.blsspainvisa.com/appointment_family.php
-// @match        *://*.blsspainvisa.com/*/appointment_family.php
-// @match        *://*.blsspainvisa.com/appointment.php
-// @match        *://*.blsspainvisa.com/*/appointment.php
-// @match        *://*.blsspainvisa.com/embassy_appointment.php
-// @match        *://*.blsspainvisa.com/*/embassy_appointment.php
-
+// @match        *://*.blsspainvisa.com/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
