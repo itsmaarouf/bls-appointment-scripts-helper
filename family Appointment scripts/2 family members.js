4a990cd810985c369e9da128ff200b906900af64
Hamza El Maarouf <78151477+itsmaaroufwork@users.noreply.github.com>
2022-05-16 14:36:03 +0200

    Update 2 family members.js

--- family Appointment scripts/2 family members.js
+++ family Appointment scripts/2 family members.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://morocco.blsspainvisa.com/*
+// @match        *://*.blsspainvisa.com/*
 // @grant        none
 // ==/UserScript==
 //06 89 200 483
