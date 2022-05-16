e78bc43a3c70b7748f4f57e255380c40482282e8
Hamza El Maarouf <78151477+itsmaaroufwork@users.noreply.github.com>
2022-05-16 13:57:44 +0200

    Update 2 family members.js

--- family Appointment scripts/2 family members.js
+++ family Appointment scripts/2 family members.js
@@ -15,57 +15,67 @@
 
 (function sayHi() {
     /partie overlay/
-    'use strict';
-    let code = document.scripts[12].text;
-    let bgn = code.indexOf("available_dates");
-    let end = code.indexOf("fullCapicity_dates");
-    let table = code.slice(bgn+20, end-9);
-    if (table !== ""){
-        let datee = table.slice(1,11);
-        let dd = datee.slice(0,2);
-        let mm = datee.slice(3,5);
-        let yyyy = datee.slice(6,10);
-        let dateeFin = [yyyy, mm, dd];
-        let finDate= dateeFin.join("-");
-        if(document.getElementById("app_date").value.length === 0 && finDate !== "")
-        {
-            $('#app_date').datepicker("update" ,finDate);
-        }else if (document.title == "504 Gateway Time-out" || document.title == "502 Bad Gateway" || document.title == "504 Gateway Timeout") {
-
-        window.setTimeout(function(){ location.reload(); }, 500);
-
-
-        /Get page (1) OPEN PAGE AND GET AUTO REPLAY/
-    } else
-        {
-        let lastNames =["last Names 1","first Names 2"],
-        firstNames =["first Names 1","first Names 2"],
-        births =["yyyy-mm-dd","yyyy-mm-dd"],
-        passNumbers =["passport No 1","passport No 2"],
-        issueDates =["yyyy-mm-dd","yyyy-mm-dd"],
-        expiryDates =["yyyy-mm-dd","yyyy-mm-dd"],
-        pptissuePalaces =["city 1","city 2"],
-        number = 2,
-        j=1;
-            setTimeout(function(){
-                for(let i=0; i<=number-1; i++){
-                document.getElementById('app_time-'+j).selectedIndex = document.getElementById('app_time-'+j).length-1;
-                document.getElementById('VisaTypeId-'+j).selectedIndex = "1";
-                document.getElementById('first_name-'+j).value=firstNames[i];
-                document.getElementById('last_name-'+j).value=lastNames[i];
-                document.getElementById('passport_number-'+j).value=passNumbers[i];
-                $('#date_of_birth-'+j).datepicker("update" ,births[i]);
-                $('#pptIssueDate-'+j).datepicker("update" ,issueDates[i]);
-                $('#pptExpiryDate-'+j).datepicker("update" ,expiryDates[i]);
-                document.getElementById('pptIssuePalace-'+j).value=pptissuePalaces[i];
-                j++
-            }
-            }, 4000)
-        }
+    let code = "";
+    if (document.scripts[11].text.indexOf("available_dates") != -1) {
+        code = document.scripts[11].text;
+        TheBigBro();
+    } else if (document.scripts[12].text.indexOf("available_dates") != -1) {
+        code = document.scripts[12].text;
+        TheBigBro();
+    } else {
+        console.log("script number is wrong");
     }
-    if(document.getElementById("app_date").value.length === 0)
-        {
-            setTimeout(function(){window.location.reload(1);}, 19 * 1000);
-        }
+    'use strict';
+     function TheBigBro() {
+        let bgn = code.indexOf("available_dates");
+        let end = code.indexOf("fullCapicity_dates");
+        let table = code.slice(bgn+20, end-9);
+        if (table !== ""){
+            let datee = table.slice(1,11);
+            let dd = datee.slice(0,2);
+            let mm = datee.slice(3,5);
+            let yyyy = datee.slice(6,10);
+            let dateeFin = [yyyy, mm, dd];
+            let finDate= dateeFin.join("-");
+            if(document.getElementById("app_date").value.length === 0 && finDate !== "")
+            {
+                $('#app_date').datepicker("update" ,finDate);
+            }else if (document.title == "504 Gateway Time-out" || document.title == "502 Bad Gateway" || document.title == "504 Gateway Timeout") {
 
+            window.setTimeout(function(){ location.reload(); }, 500);
+
+
+            /Get page (1) OPEN PAGE AND GET AUTO REPLAY/
+        } else
+            {
+            let lastNames =["last Names 1","first Names 2"],
+            firstNames =["first Names 1","first Names 2"],
+            births =["yyyy-mm-dd","yyyy-mm-dd"],
+            passNumbers =["passport No 1","passport No 2"],
+            issueDates =["yyyy-mm-dd","yyyy-mm-dd"],
+            expiryDates =["yyyy-mm-dd","yyyy-mm-dd"],
+            pptissuePalaces =["city 1","city 2"],
+            number = 2,
+            j=1;
+                setTimeout(function(){
+                    for(let i=0; i<=number-1; i++){
+                    document.getElementById('app_time-'+j).selectedIndex = document.getElementById('app_time-'+j).length-1;
+                    document.getElementById('VisaTypeId-'+j).selectedIndex = "1";
+                    document.getElementById('first_name-'+j).value=firstNames[i];
+                    document.getElementById('last_name-'+j).value=lastNames[i];
+                    document.getElementById('passport_number-'+j).value=passNumbers[i];
+                    $('#date_of_birth-'+j).datepicker("update" ,births[i]);
+                    $('#pptIssueDate-'+j).datepicker("update" ,issueDates[i]);
+                    $('#pptExpiryDate-'+j).datepicker("update" ,expiryDates[i]);
+                    document.getElementById('pptIssuePalace-'+j).value=pptissuePalaces[i];
+                    j++
+                }
+                }, 4000)
+            }
+        }
+        if(document.getElementById("app_date").value.length === 0)
+            {
+                setTimeout(function(){window.location.reload(1);}, 19 * 1000);
+            }
+        }
 })();
