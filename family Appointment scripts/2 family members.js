2653f3f157d0c1149c77be6cac08e378527cbba5
Hamza El Maarouf <78151477+itsmaaroufwork@users.noreply.github.com>
2022-05-16 15:09:48 +0200

    Update 2 family members.js

--- family Appointment scripts/2 family members.js
+++ family Appointment scripts/2 family members.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         -NAME
+// @name         individual test
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
@@ -7,72 +7,103 @@
 // @match        *://*.blsspainvisa.com/*
 // @grant        none
 // ==/UserScript==
-//06 89 200 483
-
+//---------------------------------------------------------------------------------------------------------------------------
+//////////////////////////////////////////////////////////////////////////////////////////////////////
 (function sayHi() {
-    /partie overlay/
-    let code = "";
+    'use strict';
+     let code = "";
     if (document.scripts[11].text.indexOf("available_dates") != -1) {
         code = document.scripts[11].text;
-        TheBigBro();
     } else if (document.scripts[12].text.indexOf("available_dates") != -1) {
         code = document.scripts[12].text;
-        TheBigBro();
     } else {
         console.log("script number is wrong");
     }
-
-    'use strict';
-    function TheBigBro() {
-        let bgn = code.indexOf("available_dates");
-        let end = code.indexOf("fullCapicity_dates");
-        let table = code.slice(bgn+20, end-9);
-        if (table !== ""){
-            let datee = table.slice(0,10);
-            let dd = datee.slice(0,2);
-            let mm = datee.slice(3,5);
-            let yyyy = datee.slice(6,10);
-            let dateeFin = [yyyy, mm, dd];
-            let finDate= dateeFin.join("-");
-            if(document.getElementById("app_date").value.length === 0 && finDate !== "")
-            {
-                $('#app_date').datepicker("update" ,finDate);
-            }else if (document.title == "504 Gateway Time-out" || document.title == "502 Bad Gateway" || document.title == "504 Gateway Timeout") {
-
-                window.setTimeout(function(){ location.reload(); }, 500);
-
-
-                /Get page (1) OPEN PAGE AND GET AUTO REPLAY/
-            } else
-            {
-                let lastNames =["last Names 1","first Names 2"],
-                    firstNames =["first Names 1","first Names 2"],
-                    births =["yyyy-mm-dd","yyyy-mm-dd"],
-                    passNumbers =["passport No 1","passport No 2"],
-                    issueDates =["yyyy-mm-dd","yyyy-mm-dd"],
-                    expiryDates =["yyyy-mm-dd","yyyy-mm-dd"],
-                    pptissuePalaces =["city 1","city 2"],
-                    number = 2,
-                    j=1;
-                setTimeout(function(){
-                    for(let i=0; i<=number-1; i++){
-                        document.getElementById('app_time-'+j).selectedIndex = document.getElementById('app_time-'+j).length-1;
-                        document.getElementById('VisaTypeId-'+j).selectedIndex = "1";
-                        document.getElementById('first_name-'+j).value=firstNames[i];
-                        document.getElementById('last_name-'+j).value=lastNames[i];
-                        document.getElementById('passport_number-'+j).value=passNumbers[i];
-                        $('#date_of_birth-'+j).datepicker("update" ,births[i]);
-                        $('#pptIssueDate-'+j).datepicker("update" ,issueDates[i]);
-                        $('#pptExpiryDate-'+j).datepicker("update" ,expiryDates[i]);
-                        document.getElementById('pptIssuePalace-'+j).value=pptissuePalaces[i];
-                        j++
-                    }
-                }, 4000)
-            }
-        }
-        if(document.getElementById("app_date").value.length === 0)
+    var bgn = code.indexOf("available_dates");
+    var end = code.indexOf("fullCapicity_dates");
+    var table = code.slice(bgn+20, end-9);
+    if (table !== ""){
+        var datee = table.slice(0,10);
+        var dd = datee.slice(0,2);
+        var mm = datee.slice(3,5);
+        var yyyy = datee.slice(6,10);
+        var dateeFin = [yyyy, mm, dd];
+        var finDate= dateeFin.join("-");
+        if(document.getElementById("app_date").value.length === 0 && finDate !== "")
         {
-            setTimeout(function(){window.location.reload(1);}, 19 * 1000);
+            $('#app_date').datepicker("update" ,finDate);
+
+        }else
+        {
+            //########################################################################
+            //############### THIS IS THE PART YOU MUST CHANGE #######################
+            //########################################################################
+            var lastNames ="Last Name";
+            var firstNames ="First Name";
+            var births ="yyyy-mm-dd";//Date Of Birth
+            var passNumbers ="Passport Number";//Passport Number
+            var issueDates ="yyyy-mm-dd";//Passport Issue Date
+            var expiryDates ="yyyy-mm-dd";//Passport Expiry Date
+            var pptissuePalaces ="city name";//Passport Issue Place
+            var franN ="TG700******";
+            //########################################################################
+            //########################################################################
+
+            document.getElementById('first_name').value=firstNames;
+            document.getElementById('last_name').value=lastNames;
+            $('#dateOfBirth').datepicker("update" ,births);
+            document.getElementById('passport_no').value=passNumbers;
+            $('#pptIssueDate').datepicker("update" ,issueDates);
+            $('#pptExpiryDate').datepicker("update" ,expiryDates);
+            document.getElementById('pptIssuePalace').value=pptissuePalaces;
+            var audio = new Audio('https://www.zapsplat.com/wp-content/uploads/2015/sound-effects-the-sound-pack-tree/tspt_german_ambulance_sirens_wailing_loop_041.mp3');
+            audio.play();
+            document.getElementById("app_time").selectedIndex = document.getElementById('app_time').length-1;
+            document.getElementById("VisaTypeId").selectedIndex = "1";
+            if (document.getElementById('fran') !== null){
+                document.getElementById('fran').value=franN;
+            }
+            if (document.getElementById("passportType") !== null){
+                document.getElementById("passportType").selectedIndex = "7";
+            }
+            if (document.getElementById("vasId12") !== null){
+              $("#vasId12").prop("checked", true);
+              }
         }
     }
+    if(document.getElementById("app_date").value.length === 0)
+        {
+            setTimeout(function(){window.location.reload(1);}, 30* 1000);;
+        }
+ //   ----------------------------------------------------------------------------------------------------
+ // PARTI AUTO SUBMIT
+
+ function clicksubmit() {
+    if (
+        document.getElementById("app_date").value !== "" &&
+        document.getElementById("app_time").value !== "" &&
+        document.getElementById("VisaTypeId").value !== "" &&
+        document.getElementById("first_name").value !== "" &&
+        document.getElementById('last_name').value !== "" &&
+        document.getElementById("dateOfBirth").value !== "" &&
+        document.getElementById("passportType").value !== "" &&
+        document.getElementById('passport_no').value !== "" &&
+        document.getElementById("pptIssueDate").value !== "" &&
+        document.getElementById('pptExpiryDate').value !== "" &&
+        document.getElementById("pptIssuePalace").value !== ""
+
+        ) {
+            console.log('click SUBMIT');
+            document.getElementsByClassName("btn primary-btn")[0].click();
+            }
+    }
+    var stopconsubmit = setInterval(clicksubmit, 10*1000);
+
+    document.getElementsByClassName("btn primary-btn")[0].onclick = function () {
+        'use strict'
+        console.log('STOP CLICKING SUBMIT');
+        clearTimeout(stopconsubmit);
+    }
+
+    console.log("End of code run");
 })();
