083c8e22fd59fe5e220a4b4e26e3b37ce615eeca
Hamza El Maarouf <78151477+itsmaaroufwork@users.noreply.github.com>
2022-08-02 13:40:37 +0200

    Update individual_Appointment_scripts.js

--- individual_Appointment_scripts.js
+++ individual_Appointment_scripts.js
@@ -3,8 +3,8 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
-// @author       You
-// @match        *://*.blsspainvisa.com/*
+// @author       @Itsmaarouf
+// @match        *://*.blsspainmorocco.com/*
 // @grant        none
 // ==/UserScript==
 //---------------------------------------------------------------------------------------------------------------------------
