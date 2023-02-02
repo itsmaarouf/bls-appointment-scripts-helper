7fad7ba7c6d20538c00bdeb9afc886fbc99ee003
Hamza El Maarouf <78151477+itsmaarouf@users.noreply.github.com>
2023-02-02 08:56:17 +0100

    Update and rename PaymentLink&ReceiptPDF.js to PaymentReceiptCatcher.js
    I changed the file name to clarify the role of this file

--- PaymentReceiptCatcher.js
+++ PaymentReceiptCatcher.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
-// @author       You
+// @author       Itsmaarouf
 // @match        https://payment.cmi.co.ma/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
